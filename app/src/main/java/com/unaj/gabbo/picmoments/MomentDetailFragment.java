a46e611970ddf0f6674bfc3771ac464b63bd512d
Gabriel Navarro <gabriel.navarro@hotmail.es>
2017-11-15 21:19:01 -0300

    Guardado de imagenes en sd incorporado

--- app/src/main/java/com/unaj/gabbo/picmoments/MomentDetailFragment.java
+++ app/src/main/java/com/unaj/gabbo/picmoments/MomentDetailFragment.java
@@ -7,6 +7,7 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
@@ -23,6 +24,10 @@ import android.widget.Toast;
 
 import com.unaj.gabbo.picmoments.db.SQLiteDBHelper;
 import com.unaj.gabbo.picmoments.moment.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Random;
 
 /**
  * A fragment representing a single Moment detail screen.
@@ -43,6 +48,7 @@ public class MomentDetailFragment extends Fragment {
     private Moment mItem;
 
     private int userid;
+    private Bitmap bitmapGlobal;
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
      * fragment (e.g. upon screen orientation changes).
@@ -86,6 +92,7 @@ public class MomentDetailFragment extends Fragment {
 
             byte[] imageAsByteArray = mItem.getImageAsBytes();
             Bitmap imageAsBitmap = BitmapFactory.decodeByteArray(imageAsByteArray,0, imageAsByteArray.length);
+            setBitmapGlobal (imageAsBitmap);
             moment_detailET.setText(mItem.getDescription());
             ((TextView) rootView.findViewById(R.id.moment_date)).setText(mItem.getFormatDate());
             ((ImageView) rootView.findViewById(R.id.photoDetail)).setImageBitmap(imageAsBitmap);
@@ -172,11 +179,54 @@ public class MomentDetailFragment extends Fragment {
             editDesc.setVisibility(View.INVISIBLE);
         }
 
+        ImageView photoDetail = (ImageView) rootView.findViewById(R.id.photoDetail);
+        photoDetail.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Bitmap bitmapToSave = getBitmapGlobal ();
+                return imageToExternalSD(bitmapToSave);
+            }
+        });
 
         return rootView;
     }
 
     public void setUserId(int userId) {
         this.userid = userId;
+    }
+
+    private boolean imageToExternalSD (Bitmap imageAsBitmap) {
+
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/saved_images");
+        myDir.mkdirs();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-"+ n +".jpg";
+        File file = new File (myDir, fname);
+        if (file.exists ())
+            file.delete ();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            imageAsBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+            Toast.makeText(getActivity(), ":)", Toast.LENGTH_SHORT).show();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(getActivity(), ":(", Toast.LENGTH_SHORT).show();
+
+            return false;
+        }
+    }
+
+    public void setBitmapGlobal(Bitmap bitmapGlobal) {
+        this.bitmapGlobal = bitmapGlobal;
+    }
+
+    public Bitmap getBitmapGlobal() {
+        return bitmapGlobal;
     }
 }
